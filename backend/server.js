26b6d9a78db5f9c9ea01c54ad2a8ecd028863d88
Koded0214h <coder0214h@gmail.com>
2025-11-08 11:05:27 +0100

    localhost:3000

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://ilrerai.vercel.app",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
 });
